5dc40a2fef7645617f25df3212f9284adab3fb81
Jared Taylor <jaredtaylor.99@gmail.com>
2025-02-11 07:46:52 +1300

    Add versioning system

---
+++ Source/ActorTurnInPlace/Public/System/TurnInPlaceVersioning.h
@@ -0,0 +1,15 @@
+﻿// Copyright (c) 2023 Drowning Dragons. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+#include "Misc/EngineVersionComparison.h"
+
+#ifndef UE_5_03_OR_LATER
+#if !UE_VERSION_OLDER_THAN(5, 3, 0)
+#define UE_5_03_OR_LATER 1
+#else
+#define UE_5_03_OR_LATER 0
+#endif
+#endif
